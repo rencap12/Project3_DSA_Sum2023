46661361c7719386d76f2f53d770b1b9272b29fb
mlhand <matthand@ufl.edu>
2023-08-02 18:26:24 -0400

    finished reading excel sheet data

--- main.cpp
+++ main.cpp
@@ -3,21 +3,24 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <iostream>
 
 int main(){
     std::priority_queue<std::pair<int, std::string>> MaxHeap; 
 
-    std::vector<std::pair<int, std::string>> houseData;
+    std::vector<std::pair<std::string, std::string>> houseData;
 
     //Open spreadsheet and write 101,000 tuples to houseData
     std::fstream file ("./Real_Estate_Sales_2001-2020_GL.csv", std::ios::in);
     std::vector<std::string> row;
     std::string line;
     std::string word;
+    getline(file, line); //skips first line
     if (file.is_open())
     {
-        while(getline(file, line))
+        for(int i = 0; i < 101000; i++)
         {
+            getline(file, line);
             row.clear();    
             std::stringstream str(line);
 
@@ -25,11 +28,13 @@ int main(){
             {
                 row.push_back(word);
             }
-            std::pair<int, std::string> data;
-            data.first = stoi(row[6]);
+            std::pair<std::string, std::string> data;
+            data.first = (row[6]);
             data.second = row[4];
             houseData.push_back(data);
         }
     }
+
+    std::cout << houseData[50000].second;
 
 }
